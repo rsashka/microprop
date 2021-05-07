6cc89a2e23600c3cd2d05a42c0f2c956537dab4e
Aleksandr Ryabikov <rsashka@mail.ru>
2021-05-07 12:47:22 +0300

    Fix null data and zero size

--- microprop.cpp
+++ microprop.cpp
@@ -42,8 +42,12 @@ bool Encoder::WriteAsString(KeyType id, const char *str) {
 int Encoder::callback_func(void* data, const char* buf, size_t len) {
     assert(m_data == data);
     if(m_offset + len < m_size) {
-        memcpy(&m_data[m_offset], buf, len);
-        m_offset += len;
+        if(buf && len) {
+            memcpy(&m_data[m_offset], buf, len);
+            m_offset += len;
+        } else {
+            assert(buf == nullptr && len == 0);
+        }
         return 0;
     }
     return -1;
